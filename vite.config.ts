d3721608c798b53e839b868ec324ae705a764ccf
hannahjohnston68 <171751873+hannahjohnston68@users.noreply.github.com>
2025-03-08 23:22:16 -0500

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from "vite";
-<<<<<<< HEAD
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  base: "./", // Use relative paths to fix GitHub Pages 404 errors
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "index.html", // Ensure correct entry point
-=======
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hannahrob2026/", // Use relative paths to fix GitHub Pages 404 errors
   server: {
     host: "::",
     port: 8080,
@@ -27,7 +18,11 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
->>>>>>> 4ee7548a972a378186989f507344abc210d5fabc
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: "index.html", // Ensure correct entry point
     },
   },
 }));
