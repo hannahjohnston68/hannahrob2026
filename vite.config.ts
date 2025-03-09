f96410188eb2091ade4397195c8e706bfe858f03
hannahjohnston68 <171751873+hannahjohnston68@users.noreply.github.com>
2025-03-08 22:05:13 -0500

    update vite.config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hannahrob2026/", // <-- This tells GitHub Pages where to find assets
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
