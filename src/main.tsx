80dd3c9ed8d5f3252a60e49a45c449385da4b8bd
hannahjohnston68 <171751873+hannahjohnston68@users.noreply.github.com>
2025-03-08 22:10:25 -0500

    build issue fix attempt

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,7 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found. Make sure index.html has <div id='root'></div>");
+}
